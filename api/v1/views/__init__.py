d921acbaf57d5f18706a5a5bc51334a4364dfa37
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-08-20 00:18:32 -0700

    index

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,20 +1,24 @@
-#!/usr/bin/python3
-
-from flask import Blueprint
 """
-create a route /status on the object app_views
-that returns a JSON: "status": "OK"
+A script that initialize the blueprints
 """
 from flask import Blueprint
 
+"""create a blueprint for the app_views"""
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1') 
+""" implementing import storage engine and classes"""
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.user import User
+from models.place import Place
+from models.review import Review
 
-
+"""implementing import flask views"""
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
-from api.v1.views.places_reviews import *
