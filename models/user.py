7d43c6e9fa95062f88e8b6f07a048a28e7259e0a
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-08-20 02:28:28 -0700

    user

--- models/user.py
+++ models/user.py
@@ -3,33 +3,35 @@
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
-from sqlalchemy.orm import relationship
+from hashlib import md5
+import sqlalchemy
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """Representation of a user """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+    if models.storage_t == 'db':
         __tablename__ = 'users'
-        email = Column(String(128),
-                       nullable=False)
-        password = Column(String(128),
-                          nullable=False)
-        first_name = Column(String(128),
-                            nullable=True)
-        last_name = Column(String(128),
-                           nullable=True)
-        places = relationship("Place",
-                              backref="user",
-                              cascade="all, delete-orphan")
-        reviews = relationship("Review",
-                               backref="user",
-                               cascade="all, delete-orphan")
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
     else:
         email = ""
         password = ""
         first_name = ""
         last_name = ""
+
+    def __setattr__(self, name, value):
+        """Magic method for hashing password"""
+        if name == "password":
+            super(User, self).__setattr__(name,
+                                          md5(value.encode()).hexdigest())
+        else:
+            super(User, self).__setattr__(name, value)
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
