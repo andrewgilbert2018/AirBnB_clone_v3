a1b7b53df0baf607333e5b99d0b1a46b357d94eb
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-08-20 02:46:41 -0700

    states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,71 +1,85 @@
 #!/usr/bin/python3
-"""Creating a new view for state objects"""
+"""
 
+Flask web server creation to handle api petition-requests
+
+"""
+from flask import jsonify, abort
+from flask import request
 from api.v1.views import app_views
-from flask import jsonify, request, abort, make_response
 from models import storage
-from models.state import State
-import json
+from models.engine.db_storage import classes
 
 
 @app_views.route('/states', strict_slashes=False, methods=['GET'])
-def handle_states():
-    """return states in json"""
-    if request.method == 'GET':
-        states = []
-        for state in storage.all('State').values():
-            states.append(state.to_dict())
-        return jsonify(states)
+def all_states():
+    """
+    Retrieves the list of all State objects
+    """
+    objects = storage.all("State")
+    list_obj = []
+    for obj in objects.values():
+        list_obj.append(obj.to_dict())
+    return jsonify(list_obj)
 
 
 @app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET'])
-def fetch_single_state(state_id):
-    """fetch a single state"""
-    state = {}
-    for st in storage.all('State').values():
-        if st.to_dict().get('id') == state_id:
-            state = st.to_dict()
-    if not state:
+def some_state(state_id):
+    """
+    Retrieves a State object if id is linked to some State object
+    """
+    some_objs = storage.get(classes["State"], state_id)
+    if some_objs is None:
         abort(404)
-    return jsonify(state)
-
-
-@app_views.route('/states', strict_slashes=False, methods=['POST'])
-def post_state():
-    """function to post state"""
-    if not request.get_json():
-        return jsonify({"error": "Not a JSON"}), 400
-    state = request.get_json()
-    if not state['name']:
-        return jsonify({"error": "Missing name"}), 400
-    newInstance = State(state)
-    newInstance.name = state.get("name")
-    storage.new(newInstance)
-    storage.save()
-    return jsonify(newInstance.to_dict()), 201
-
-
-@app_views.route('/states/<state_id>', strict_slashes=False, methods=['PUT'])
-def put_state(state_id):
-    """Function that update state"""
-    if not request.get_json():
-        return jsonify({"error": "Not a JSON"}), 400
-    obj = storage.get("State", state_id)
-    if not obj:
-        abort(404)
-    requestObj = request.get_json()
-    obj.name = requestObj["name"]
-    obj.save()
-    return jsonify(obj.to_dict()), 200
+    some_objs = some_objs.to_dict()
+    return jsonify(some_objs)
 
 
 @app_views.route('/states/<state_id>',
                  strict_slashes=False, methods=['DELETE'])
-def delete_state(state_id):
-    """Function that delete state"""
-    obj = storage.get("State", state_id)
-    if not obj:
+def del_state(state_id):
+    """
+    Deletes a State object if id is linked to some State object
+    """
+    some_objs = storage.get(classes["State"], state_id)
+    if some_objs is None:
         abort(404)
-    storage.delete(obj)
+    storage.delete(some_objs)
     storage.save()
-    return jsonify({}), 200
+    return jsonify({})
+
+
+@app_views.route('/states/', strict_slashes=False, methods=['POST'])
+def post_state():
+    """
+    Create a new State object
+    """
+    data_json = request.get_json(force=True, silent=True)
+    if (type(data_json) is not dict):
+        abort(400, "Not a JSON")
+    if "name" in data_json:
+        new_state = classes["State"](**data_json)
+        storage.new(new_state)
+        storage.save()
+        return jsonify(new_state.to_dict()), 201
+    else:
+        abort(400, "Missing name")
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['PUT'])
+def put_state(state_id):
+    """
+    Update a State object
+    """
+    obj = storage.get(classes["State"], state_id)
+    if obj is None:
+        abort(404)
+    data_json = request.get_json(force=True, silent=True)
+    if (type(data_json) is not dict):
+        abort(400, "Not a JSON")
+    for key, value in data_json.items():
+        if key in ["id", "created_at", "updated_at"]:
+            continue
+        setattr(obj, key, value)
+    storage.save()
+    return jsonify(obj.to_dict())
